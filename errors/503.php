6530957b7d8b3d54c02498fda3d33c3bc1294929
Dmitry Karpenko <69617058+dimkarpenko@users.noreply.github.com>
2021-04-14 20:20:07 +0500

    Add files via upload

---
+++ errors/503.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>503 Service Unavailable</title>
+	<link rel="shortcut icon" href="https://intprogramming.000webhostapp.com/images/file-code-icon.png" type="image/x-icon">
+</head>
+<body>
+	<center>
+		<style type="text/css">
+			* {
+				font-family : "Courier New", Courier, monospace;
+			    
+			}
+			
+			
+			img{
+			    margin-left:120%;
+			}
+		</style>
+	<h1>На сервере ведутся технические работы</h1>
+	<h2>Приносим свои извинения!</h2>
+	</center>
+	<?php include "snake-game.php";?>
+</body>
+</html>
